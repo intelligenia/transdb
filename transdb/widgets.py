30d55f54bc1b21a31d08083fa10abe55b7b8f747
Diego J. Romero <diego@intelligenia.com>
2015-11-24 14:07:08 +0100

    Change 'django.forms.util' to 'django.forms.utils'

--- transdb/widgets.py
+++ transdb/widgets.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_unicode
 from django.utils.html import conditional_escape
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 
 class TransCharWidget(Widget):
     '''
